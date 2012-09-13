ca0118e06b0c10d66311f2b5f9984ad6826a96d2
ggear <graham.gear@gmail.com>
2012-09-13 22:29:10 +0100

    First pass impl

---
+++ src/main/java/com/cloudera/whirr/kerberos/BaseHandler.java
@@ -0,0 +1,46 @@
+/**
+ * Licensed to Cloudera, Inc. under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  Cloudera, Inc. licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cloudera.whirr.kerberos;
+
+import static org.jclouds.scriptbuilder.domain.Statements.call;
+
+import java.io.IOException;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.whirr.ClusterSpec;
+import org.apache.whirr.service.ClusterActionEvent;
+import org.apache.whirr.service.ClusterActionHandlerSupport;
+
+public abstract class BaseHandler extends ClusterActionHandlerSupport {
+
+	protected Configuration getConfiguration(ClusterSpec spec) throws IOException {
+		return getConfiguration(spec, "whirr-kerberos-default.properties");
+	}
+
+	@Override
+	protected void beforeBootstrap(ClusterActionEvent event) throws IOException {
+		addStatement(event, call("configure_hostnames"));
+		addStatement(event, call("install_kerberos_client"));
+	}
+
+	@Override
+	protected void beforeConfigure(ClusterActionEvent event) throws IOException {
+		addStatement(event, call("configure_kerberos_client"));
+	}
+
+}
